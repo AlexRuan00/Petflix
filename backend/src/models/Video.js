8c4d623fea0bf746e4b7a982524b2836f18b85e6
Álex <alex.ruan.ribeiro@gmail.com>
2023-09-25 09:51:13 -0300

    feat: Adding connected api to mongoDB

---
+++ backend/src/models/Video.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+const videoSchema = new mongoose.Schema ({
+    id: { type: mongoose.Schema.Types.ObjectId },
+    url: { type: String, required: true },
+    category: { type: String, required: true  }
+}, { versionKey: false });
+
+const video = mongoose.model("videos", videoSchema);
+
+export default video;
