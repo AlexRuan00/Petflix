1222b6ac39404cb837754eab585ab71fd86a1d1e
Álex <alex.ruan.ribeiro@gmail.com>
2023-09-27 17:00:17 -0300

    feat: adding image to video preview

--- frontend/src/components/VideoCard/index.jsx
+++ frontend/src/components/VideoCard/index.jsx
@@ -1,9 +1,9 @@
 import './VideoCard.css';
 
-const VideoCard = () => {
+const VideoCard = ({img}) => {
     return (
         <div className="thumbnail">
-          <img src="https://i.pinimg.com/236x/9a/a8/05/9aa8051f438e26820e754322a687c082.jpg" alt="Miniatura do vídeo" />
+          <img src={img} alt="Miniatura do vídeo" />
         </div>
       );
 }
